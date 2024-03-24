fe470611aab544ebba3eb514582ec3f78f69dde4
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2024-03-24 10:32:56 +0000

    Deploying to gh-pages from @ AleksandrNebesnyi/goit-js-hw-09-2024@303872743cd2895536bab12baa496ccc97eac720 🚀

---
+++ 01-color-switcher.7bad8d3a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));let o=null;
+//# sourceMappingURL=01-color-switcher.7bad8d3a.js.map
